09374eb4a05f7fbe2dc54c2b689116292a8c2b9f
Simon Vrachliotis <simon@simonswiss.com>
2018-12-10 07:04:05 +1100

    removing dependency on document with striptags

--- src/utils/stripHtml.js
+++ src/utils/stripHtml.js
@@ -1,8 +1,5 @@
+import striptags from 'striptags'
+
 export function stripHtml(html) {
-  // Create a new div element
-  var temporalDivElement = document.createElement('div')
-  // Set the HTML content with the providen
-  temporalDivElement.innerHTML = html
-  // Retrieve the text property of the element (cross-browser support)
-  return temporalDivElement.textContent || temporalDivElement.innerText || ''
+  return striptags(html)
 }
