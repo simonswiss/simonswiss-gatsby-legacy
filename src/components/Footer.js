eb231b66b998ab80957b585708b9bb274abfba5a
Simon Vrachliotis <simon@simonswiss.com>
2018-12-10 09:19:48 +1100

    margin top on Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { css } from 'emotion'
 
 export default () => (
-  <footer className={css(tw`max-w-lg mx-auto px-4 mb-8 w-full`)}>
+  <footer className={css(tw`max-w-lg mx-auto px-4 my-8 w-full`)}>
     <p className={css(tw`text-grey-darker`)}>
       Built with 😍, <a href="https://www.gatsbyjs.org/">Gatsby</a> and{' '}
       <a href="https://tailwindcss.com">Tailwind CSS</a>.
