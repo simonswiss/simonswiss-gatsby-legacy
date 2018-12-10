d2d4eccc4c79b367c1a00da4489f52e3711787d6
Simon Vrachliotis <simon@simonswiss.com>
2018-12-10 13:55:12 +1100

    adding 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import Layout from '../components/Layout'
+import Helmet from 'react-helmet'
+import { css } from 'emotion'
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
-)
+class Page extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <Helmet
+          htmlAttributes={{ lang: 'en' }}
+          meta={[
+            {
+              name: 'description',
+              content: 'Oops! We have a 404 Error!',
+            },
+          ]}
+          title="404 | simonswiss.com"
+        />
+        <article>
+          <h1
+            className={css(
+              tw`mb-4 font-black leading-tight text-3xl lg:text-4xl`
+            )}
+          >
+            Oops - 404 Error!
+          </h1>
+          <p>Oh no! Looks like you've hit a page that doesn't exist.</p>
+        </article>
+      </Layout>
+    )
+  }
+}
 
-export default NotFoundPage
+export default Page
