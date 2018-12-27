fe31f60e51082fbd677226a926c1296e6d1c2e62
Simon Vrachliotis <simon@simonswiss.com>
2018-12-27 21:03:43 +1100

    active link styling with getProps

--- src/components/sidebar/Navigation.js
+++ src/components/sidebar/Navigation.js
@@ -2,44 +2,36 @@ import React from 'react'
 import { css } from 'emotion'
 import { Link } from 'gatsby'
 
+const NavLink = props => (
+  <li className={css(tw`mb-1`)}>
+    <Link
+      to={props.to}
+      getProps={props => {
+        console.log('props', props)
+        return {
+          className: props.isCurrent
+            ? css(
+                tw`text-purple-darker no-underline inline-block border-b-2 border-purple-dark`
+              )
+            : css(
+                tw`text-purple no-underline inline-block border-b-2 border-transparent`
+              ),
+        }
+      }}
+    >
+      {props.children}
+    </Link>
+  </li>
+)
+
 class Navigation extends React.Component {
   render() {
     return (
       <ul className={css(tw`list-reset mb-6`)}>
-        <li className={css(tw`mb-1`)}>
-          <Link
-            className={css(tw`text-purple no-underline hover:underline`)}
-            to="/"
-          >
-            Blog
-          </Link>
-        </li>
-        <li className={css(tw`mb-1`)}>
-          <Link
-            className={css(tw`text-purple no-underline hover:underline`)}
-            to="/talks"
-          >
-            Talks
-          </Link>
-        </li>
-
-        <li className={css(tw`mb-1`)}>
-          <Link
-            className={css(tw`text-purple no-underline hover:underline`)}
-            to="/about"
-          >
-            About
-          </Link>
-        </li>
-
-        <li className={css(tw`mb-1`)}>
-          <Link
-            className={css(tw`text-purple no-underline hover:underline`)}
-            to="/contact"
-          >
-            Contact
-          </Link>
-        </li>
+        <NavLink to="/">Blog</NavLink>
+        <NavLink to="/talks">Talks</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
       </ul>
     )
   }
