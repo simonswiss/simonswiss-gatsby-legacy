89be97d96b3a8bfc631525326644bcd2c0c2f03f
Simon Vrachliotis <simon@simonswiss.com>
2020-04-27 22:05:38 +1000

    adding prism-react-renderer + test post, and also blogquote styles

--- src/components/long-form/code-block.js
+++ src/components/long-form/code-block.js
@@ -1,7 +1,22 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import tw from 'tailwind.macro';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import theme from 'prism-react-renderer/themes/nightOwl';
 
-export const CodeBlock = ({ block }) => (
-  <pre>
-    <code>{block.codeBlock}</code>
-  </pre>
+export const CodeBlock = ({ language = 'jsx', code }) => (
+  <Highlight {...defaultProps} theme={theme} code={code} language={language}>
+    {({ style, tokens, getLineProps, getTokenProps }) => (
+      <pre css={tw`my-8 p-4 rounded shadow-lg`} style={style}>
+        {tokens.map((line, i) => (
+          <div {...getLineProps({ line, key: i })}>
+            <span css={tw`mr-4 opacity-50`}>{i + 1}</span>
+            {line.map((token, key) => (
+              <span {...getTokenProps({ token, key })} />
+            ))}
+          </div>
+        ))}
+      </pre>
+    )}
+  </Highlight>
 );
