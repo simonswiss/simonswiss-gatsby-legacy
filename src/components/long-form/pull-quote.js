ad039fe7fd3352ac7a9a52c71af6baa16dc4f4ca
Simon Vrachliotis <simon@simonswiss.com>
2020-04-28 08:33:05 +1000

    Prism for code block (#10)
    * adding prism-react-renderer + test post, and also blogquote styles
    
    * removing test post
    
    Co-authored-by: Simon Vrachliotis <svrachliotis@atlassian.com>

--- src/components/long-form/pull-quote.js
+++ src/components/long-form/pull-quote.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Link } from 'gatsby';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import tw from 'tailwind.macro';
 
-export const PullQuote = ({ block }) => (
-  <div>
-    <blockquote>{block.quote}</blockquote>
-    {block.authorLink ? (
-      <Link to={block.authorLink}>
-        <cite>{block.authorName}</cite>
-      </Link>
+export const PullQuote = ({ link, author, children }) => (
+  <div css={tw`border-l-4 border-purple-500 pl-4 my-10`}>
+    <blockquote css={tw`font-serif italic text-2xl`}>{children}</blockquote>
+    {link ? (
+      <a href={link}>
+        <cite>{author}</cite>
+      </a>
     ) : (
-      <cite>{block.authorName}</cite>
+      <cite css={tw`block mt-3 not-italic text-gray-700`}>~ {author}</cite>
     )}
   </div>
 );
